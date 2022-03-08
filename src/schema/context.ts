783feb726eb2be82e35a3d7058b2fdc85e7dfa77
Octanium <octaniumcontact@gmail.com>
2022-03-08 16:18:31 +0100

    Add redis

--- src/schema/context.ts
+++ src/schema/context.ts
@@ -2,15 +2,19 @@ import { PrismaClient } from '@prisma/client'
 import { MicroRequest } from 'apollo-server-micro/dist/types'
 import { ServerResponse } from 'http'
 import {ContextFunction} from "apollo-server-core";
+import redis, {RedisClientType} from 'redis';
 
 const prisma = new PrismaClient()
 
 export interface Context {
   prisma: PrismaClient
+  cache: RedisClientType
   res: ServerResponse
   req: MicroRequest
 }
 
 export const createContext: ContextFunction<Context> = ({ res, req }) => {
-  return { prisma, res, req }
+  const cache = redis.createClient();
+
+  return { prisma, cache, res, req }
 }

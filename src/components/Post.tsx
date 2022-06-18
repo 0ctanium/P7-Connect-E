0b27d2ab806af673efdae9dc59216a5f299cb585
Octanium <octaniumcontact@gmail.com>
2022-06-18 20:46:01 +0200

    style: 💄 improve style of posts

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,28 +1,50 @@
-import React, {FC} from 'react'
+import React, {FC, useMemo} from 'react'
 import {Post as PostType, useGetGroupPostsQuery, useGetUserPostsQuery} from "generated/graphql";
 import {LoadingSpinner} from "./LoadingSpinner";
 import Image from "next/image";
 import {Avatar} from "./Avatar";
+import moment, {MomentInput} from "moment";
+import {Tooltip} from "./Tooltip";
+
+const PostDate: FC<{ date: MomentInput}> = ({ date }) => {
+    const calendar = useMemo(() => moment(date).calendar({
+                lastDay : "[hier à] LT",
+                sameDay : "LT",
+                nextDay : "[demain à] LT",
+                nextWeek: "dddd [prochain à] LT",
+                lastWeek: "dddd [dernier à] LT",
+                sameElse: function(now) {
+                    if(moment(this as MomentInput).year() != moment(now).year()) {
+                        return "D MMMM YYYY"
+                    } else {
+                        return "[le] D MMMM [à] LT"
+                    }
+                }
+            }), [date])
+    const completeDate = useMemo(() => moment(date).format("[le] dddd D MMMM YYYY [à] LT"), [date])
+
+    return <Tooltip render={completeDate}>{calendar}</Tooltip>
+
+}
 
 export const Post: FC<{ post: PostType}> = ({ post }) => {
     const { author, group } = post
 
-    return <div>
+    return <div className="bg-white rounded shadow px-8 py-3">
+        <div>
+            <PostDate date={post.createdAt} />
+        </div>
         {author && (
             <div>
                 <Avatar user={author} showTooltip hideStatus />
                 <p>{author?.name}</p>
             </div>
         )}
-        {post.createdAt}
 
         {post.text}
         {/*{JSON.stringify(post, null, 4)}*/}
     </div>
 }
-
-
-
 
 export const GroupPosts: FC<{ groupId: string }> = ({ groupId }) => {
     const { data, loading} = useGetGroupPostsQuery({ variables: { id: groupId } })
@@ -31,7 +53,7 @@ export const GroupPosts: FC<{ groupId: string }> = ({ groupId }) => {
 
     if(!data) return <p>Ce groupe ne contient aucun post</p>
 
-    return <div>
+    return <div className="max-w-xl mx-auto space-y-4">
         {data.posts.map(post => <Post post={post as PostType} key={post.id}/>)}
     </div>
 }

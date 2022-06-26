d670ee308c4bac87e75f0884846b2b77fdccefb8
Octanium <octaniumcontact@gmail.com>
2022-06-26 22:11:34 +0200

    feat: 🎸 add mutations for reactions

--- src/schema/types/reaction.ts
+++ src/schema/types/reaction.ts
@@ -47,103 +47,51 @@ export const ReactionMutations = extendType({
         if (!session?.user?.id)
           throw new AuthenticationError('You must be authenticated');
 
-        return ctx.prisma.post.create({
-          data: {
-            group: {
+        return ctx.prisma.reaction.upsert({
+          where: {
+            postId_userId: {
+              postId: args.post,
+              userId: session.user.id,
+            },
+          },
+          create: {
+            post: {
               connect: {
                 id: args.group,
               },
             },
-            author: {
+            user: {
               connect: {
                 id: session.user.id,
               },
             },
-            text: args.text,
-            // media
+            icon: args.icon,
+          },
+          update: {
+            icon: args.icon,
           },
         });
       },
     });
 
-    t.field('editPost', {
+    t.field('removePostReaction', {
       type: 'Post',
       args: {
         post: nonNull('ID'),
-        text: nonNull('String'),
-        // media: arg({
-        //   type: list('Upload'),
-        // }),
       },
       async resolve(root, args, ctx) {
         const session = await getSession(ctx);
         if (!session?.user?.id)
           throw new AuthenticationError('You must be authenticated');
 
-        const postToDelete = await ctx.prisma.post.findUnique({
+        return ctx.prisma.reaction.delete({
           where: {
-            id: args.post,
+            postId_userId: {
+              postId: args.post,
+              userId: session.user.id,
+            },
           },
         });
-
-        if (!postToDelete) {
-          throw new ApolloError('Post not found');
-        }
-
-        if (
-          postToDelete.authorId !== session.user.id &&
-          session.user.role !== Role.MODERATOR &&
-          session.user.role !== Role.ADMIN
-        ) {
-          throw new ApolloError("You don't have the permission to do this");
-        }
-
-        return ctx.prisma.post.update({
-          where: {
-            id: args.post,
-          },
-          data: {
-            text: args.text,
-            // media
-          },
-        });
-      },
-    });
-
-    t.field('deletePost', {
-      type: 'ID',
-      args: {
-        post: nonNull('ID'),
-      },
-      async resolve(root, args, ctx) {
-        const session = await getSession(ctx);
-        if (!session?.user)
-          throw new AuthenticationError('You must be authenticated');
-
-        const postToDelete = await ctx.prisma.post.findUnique({
-          where: {
-            id: args.post,
-          },
-        });
-
-        if (!postToDelete) {
-          throw new ApolloError('Post not found');
-        }
-
-        if (
-          postToDelete.authorId !== session.user.id &&
-          session.user.role !== Role.MODERATOR &&
-          session.user.role !== Role.ADMIN
-        ) {
-          throw new ApolloError("You don't have the permission to do this");
-        }
-
-        await ctx.prisma.post.delete({
-          where: {
-            id: args.post,
-          },
-        });
-        return args.post;
       },
     });
   },
